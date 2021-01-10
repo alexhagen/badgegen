6862e026d4d442bf64bb4a907daf25110c23b657
Alex Hagen <alexhagen6@gmail.com>
2021-01-10 09:08:25 -0800

    found the right svgwrite commands for a path and curve

--- badgegen/__init__.py
+++ badgegen/__init__.py
@@ -4,7 +4,10 @@ import svgwrite
 class BadgeGenerator:
     def __init__(self):
         dwg = svgwrite.Drawing('badge.svg', profile='tiny')
-        dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
+        path = svgwrite.path.Path()
+        path.push(*elements)
+        path.push_arc(t)
+        dwg.add(path)
         dwg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
         dwg.save()
 
