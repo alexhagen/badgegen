6dc17b03aa2fcc64b5fd0bf4b50ba66f5d21e6e5
Alex Hagen <alexander.hagen@pnnl.gov>
2021-01-12 07:59:15 -0800

    got the curve figured out

--- badgegen/__init__.py
+++ badgegen/__init__.py
@@ -1,12 +1,14 @@
 import argparse
 import svgwrite
+from svgwrite.shapes import Line
 
 class BadgeGenerator:
     def __init__(self):
         dwg = svgwrite.Drawing('badge.svg', profile='tiny')
         path = svgwrite.path.Path()
-        path.push(*elements)
-        path.push_arc(t)
+        #path.push(Line(start=(0,0), end=(0, 2.5)))
+        #path.push_arc(target=(0.5, 3.0), rotation=0.0, r=0.5, absolute=True, large_arc=False)
+        path.push('M 0,2.5 c 0,2.75 0.25,3.0 0.5,3.0')
         dwg.add(path)
         dwg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
         dwg.save()
@@ -15,4 +17,4 @@ class BadgeGenerator:
         pass
 
 if __name__ == "__main__":
-    pass
+    badge_generator = BadgeGenerator()
