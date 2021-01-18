e67440e815fa0a9be82219f8d08749e1b19883a9
Alex Hagen <alexander.hagen@pnnl.gov>
2021-01-18 08:47:09 -0800

    object orienting the left and right side code

--- badgegen/__init__.py
+++ badgegen/__init__.py
@@ -7,34 +7,35 @@ class BadgeGenerator:
     """Badge Generator."""
     def __init__(self):
         """Initialize the badge generator and write out the outline."""
-        dwg = svgwrite.Drawing('badge.svg', profile='tiny')
-        path = svgwrite.path.Path()
-        w = 75.0
-        r = 5.0
-        h = 25.0
-        path.push(f'M 0.0,{h - r} C 0.0,{h - r/2} {r/2.0},{h} {r},{h}')
-        path.push(f'L {r},{h} {w},{h}')
-        path.push(f'L {w},{h} {w},0.0')
-        path.push(f'L {w},0 {r},0')
-        path.push(f'C {r/2.0},0 0,{r/2.0} 0,{r}')
-        path.push('Z')
-        dwg.add(path)
-        path = svgwrite.path.Path(fill='red')
-        lw = w
-        rw = 150.0
-        path.push(f'M {lw + rw - r},{h} C {lw + rw - r/2.0},{h} ' +
-                  f'{lw + rw},{h - r/2.0} {lw + rw},{h - r}')
-        path.push(f'L {lw + rw},{h - r} {lw + rw},{r}')
-        path.push(f'C {lw + rw},{r/2.0} {lw + rw - r/2.0},0 {lw + rw - r},0')
-        path.push(f'L {lw + rw - r},0 {lw},0')
-        path.push(f'L {lw},0 {lw},{h}')
-        path.push('Z')
-        dwg.add(path)
-        dwg.add(dwg.text('Test', insert=(lw/2.0, hw/2.0), fill='red'))
-        dwg.save()
+        self.dwg = svgwrite.Drawing('badge.svg', profile='tiny')
+        self.left_rectangle()
+        self.right_rectangle()
+        self.dwg.add(self.dwg.text('Test', insert=(75.0/2.0, 25.0/2.0), fill='red'))
+        self.dwg.save()
 
     def __call__(self):
         pass
+
+    def left_rectangle(self, width=75.0, height=25.0, radius=5.0):
+        path = svgwrite.path.Path()
+        path.push(f'M 0.0,{height - radius} C 0.0,{height - radius/2} {radius/2.0},{height} {radius},{height}')
+        path.push(f'L {radius},{height} {width},{height}')
+        path.push(f'L {width},{height} {width},0.0')
+        path.push(f'L {width},0 {radius},0')
+        path.push(f'C {radius/2.0},0 0,{radius/2.0} 0,{radius}')
+        path.push('Z')
+        self.dwg.add(path)
+
+    def right_rectangle(self, width=150.0, height=25.0, radius=5.0, corner=75.0):
+        path = svgwrite.path.Path(fill='red')
+        path.push(f'M {corner + width - radius},{height} C {corner + width - radius/2.0},{height} ' +
+                  f'{corner + width},{height - radius/2.0} {corner + width},{height - radius}')
+        path.push(f'L {corner + width},{height - radius} {corner + width},{radius}')
+        path.push(f'C {corner + width},{radius/2.0} {corner + width - radius/2.0},0 {corner + width - radius},0')
+        path.push(f'L {corner + width - radius},0 {corner},0')
+        path.push(f'L {corner},0 {corner},{height}')
+        path.push('Z')
+        self.dwg.add(path)
 
 if __name__ == "__main__":
     badge_generator = BadgeGenerator()
