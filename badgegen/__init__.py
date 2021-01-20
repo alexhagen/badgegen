a2a08ab139931c1b3eb7b20b03fde240a2142926
Alex Hagen <alexander.hagen@pnnl.gov>
2021-01-20 10:00:49 -0800

    got the text vertically centered

--- badgegen/__init__.py
+++ badgegen/__init__.py
@@ -3,22 +3,30 @@ import argparse
 import svgwrite
 from svgwrite.shapes import Line
 
+def points_to_pixels(pts):
+    return 4.0 * pts / 3.0
+
 class BadgeGenerator:
     """Badge Generator."""
     def __init__(self, left_width=150, right_width=150, left_text='test',
-                 right_text='100%', height=50, radius=5):
+                 right_text='100%', height=50, radius=5, left_stroke='#000000',
+                 left_fill='#000000', left_text_color='#FFFFFF',
+                 right_stroke='#000000', right_fill='#FFFFFF', right_text_color='#000000'):
         """Initialize the badge generator and write out the outline."""
         self.dwg = svgwrite.Drawing('badge.svg', profile='tiny')
-        self.left_rectangle(left_width, height, radius)
-        self.right_rectangle(right_width, height, radius, left_width)
-        self.dwg.add(self.dwg.text(left_text, insert=(left_width/2.0, height/2.0), fill='red'))
-        self.dwg.add(self.dwg.text(right_text, insert=(left_width + right_width/2.0, height/2.0), fill='red'))
+        self.left_rectangle(left_width, height, radius, left_stroke, left_fill)
+        self.right_rectangle(right_width, height, radius, left_width, right_stroke, right_fill)
+        font_size = 16
+        text = self.dwg.add(self.dwg.text("", insert=(left_width/2.0, height/2.0 + points_to_pixels(font_size)/4.0)))
+        text.add(self.dwg.tspan(left_text, fill=left_text_color, text_anchor="middle", font_size=font_size))
+        text = self.dwg.add(self.dwg.text("", insert=(left_width + right_width/2.0, height/2.0 + points_to_pixels(font_size)/4.0)))
+        text.add(self.dwg.tspan(right_text, fill=right_text_color, text_anchor="middle", font_size=font_size))
         
     def __call__(self):
         self.dwg.save()
 
-    def left_rectangle(self, width=75.0, height=25.0, radius=5.0):
-        path = svgwrite.path.Path()
+    def left_rectangle(self, width=75.0, height=25.0, radius=5.0, stroke='#000000', fill='#000000'):
+        path = svgwrite.path.Path(stroke=stroke, fill=fill)
         path.push(f'M 0.0,{height - radius} C 0.0,{height - radius/2} {radius/2.0},{height} {radius},{height}')
         path.push(f'L {radius},{height} {width},{height}')
         path.push(f'L {width},{height} {width},0.0')
@@ -27,8 +35,8 @@ class BadgeGenerator:
         path.push('Z')
         self.dwg.add(path)
 
-    def right_rectangle(self, width=150.0, height=25.0, radius=5.0, corner=75.0):
-        path = svgwrite.path.Path(fill='red')
+    def right_rectangle(self, width=150.0, height=25.0, radius=5.0, corner=75.0, stroke='#000000', fill='#000000'):
+        path = svgwrite.path.Path(stroke=stroke, fill=fill)
         path.push(f'M {corner + width - radius},{height} C {corner + width - radius/2.0},{height} ' +
                   f'{corner + width},{height - radius/2.0} {corner + width},{height - radius}')
         path.push(f'L {corner + width},{height - radius} {corner + width},{radius}')
@@ -46,8 +54,20 @@ def _get_argparser():
                         help='Width of the left cell, ``None`` if fit to text')
     parser.add_argument('--left-text', default='test', nargs='?',
                         help='Text for the left cell. Can be python format string')
+    parser.add_argument('--left-stroke', default='#000000', nargs='?',
+                        help='Outline color for left cell.')
+    parser.add_argument('--left-fill', default='#000000', nargs='?',
+                        help='Fill color for left cell.')
+    parser.add_argument('--left-text-color', default='#FFFFFF', nargs='?',
+                        help='Text color for left cell.')
     parser.add_argument('--right-text', default='100%', nargs='?',
                         help='Text for the right cell.  Can be python format string')
+    parser.add_argument('--right-stroke', default='#000000', nargs='?',
+                        help='Outline color for right cell.')
+    parser.add_argument('--right-fill', default='#FFFFFF', nargs='?',
+                        help='Fill color for right cell.')
+    parser.add_argument('--right-text-color', default='#000000', nargs='?',
+                        help='Text color for right cell.')
     return parser
 
 def _run_cli():
