45cd6336cec12afb9a5929e7e813f5b351515b0d
Alex Hagen <alexander.hagen@pnnl.gov>
2021-01-23 11:15:41 -0800

    documentation

--- badgegen/__init__.py
+++ badgegen/__init__.py
@@ -4,6 +4,11 @@ import svgwrite
 from svgwrite.shapes import Line
 
 def points_to_pixels(pts):
+    """Approximate the number of pixels if given pts.
+
+    :param float pts: the measure in points to convert
+    :returns: number of pixels
+    """
     return 4.0 * pts / 3.0
 
 class BadgeGenerator:
@@ -23,9 +28,10 @@ class BadgeGenerator:
         if height is None:
             height = font_size * 1.5
         self.left_rectangle(left_width, height, radius, left_stroke, left_fill)
-        self.right_rectangle(right_width, height, radius, left_width, right_stroke, right_fill)
+        self.right_rectangle(right_width, height, radius, left_width,
+                             right_stroke, right_fill)
         font_size = 16
-        text = self.dwg.add(self.dwg.text("", insert=(left_width/2.0, height/2.0 + points_to_pixels(font_size)/4.0)))
+        text = self.dwg.add(self.dwg.text("", insert=(left_width/2.0,height/2.0 + points_to_pixels(font_size)/4.0)))
         text.add(self.dwg.tspan(left_text, fill=left_text_color, text_anchor="middle", font_size=font_size))
         text = self.dwg.add(self.dwg.text("", insert=(left_width + right_width/2.0, height/2.0 + points_to_pixels(font_size)/4.0)))
         text.add(self.dwg.tspan(right_text, fill=right_text_color, text_anchor="middle", font_size=font_size))
@@ -33,9 +39,19 @@ class BadgeGenerator:
     def __call__(self):
         self.dwg.save()
 
-    def left_rectangle(self, width=75.0, height=25.0, radius=5.0, stroke='#000000', fill='#000000'):
+    def left_rectangle(self, width=75.0, height=25.0, radius=5.0,
+                       stroke='#000000', fill='#000000'):
+        """"Creates a rectangle with rounded left corners.
+
+        :param float width: width of the rectangle in pts. Default ``75.0``
+        :param float height: height of the rectangle in pts. Default ``25.0``
+        :param float radius: radius of rounded corners in pts. Default ``5.0``
+        :param str stroke: color of the rectangle outline. Default ``#000000``
+        :param str fill: color of the rectangle fill. Default ``#FFFFFF``.
+        """
         path = svgwrite.path.Path(stroke=stroke, fill=fill)
-        path.push(f'M 0.0,{height - radius} C 0.0,{height - radius/2} {radius/2.0},{height} {radius},{height}')
+        path.push(f'M 0.0,{height - radius} C 0.0,{height - radius/2} ' +
+                  f'{radius/2.0},{height} {radius},{height}')
         path.push(f'L {radius},{height} {width},{height}')
         path.push(f'L {width},{height} {width},0.0')
         path.push(f'L {width},0 {radius},0')
@@ -43,12 +59,28 @@ class BadgeGenerator:
         path.push('Z')
         self.dwg.add(path)
 
-    def right_rectangle(self, width=150.0, height=25.0, radius=5.0, corner=75.0, stroke='#000000', fill='#000000'):
+    def right_rectangle(self, width=150.0, height=25.0, radius=5.0,
+                        corner=75.0, stroke='#000000', fill='#000000'):
+        """"Creates a rectangle with rounded right corners.
+
+        :param float width: width of the rectangle in pts. Default ``150.0``
+        :param float height: height of the rectangle in pts. Default ``25.0``
+        :param float radius: radius of rounded corners in pts. Default ``5.0``
+        :param float corner: x-coordinate of the left corner in pts. Default
+            ``75.0``
+        :param str stroke: color of the rectangle outline. Default ``#000000``
+        :param str fill: color of the rectangle fill. Default ``#FFFFFF``.
+        """
         path = svgwrite.path.Path(stroke=stroke, fill=fill)
-        path.push(f'M {corner + width - radius},{height} C {corner + width - radius/2.0},{height} ' +
-                  f'{corner + width},{height - radius/2.0} {corner + width},{height - radius}')
-        path.push(f'L {corner + width},{height - radius} {corner + width},{radius}')
-        path.push(f'C {corner + width},{radius/2.0} {corner + width - radius/2.0},0 {corner + width - radius},0')
+        path.push(f'M {corner + width - radius},{height} ' +
+                  f'C {corner + width - radius/2.0},{height} ' +
+                  f'{corner + width},{height - radius/2.0} ' +
+                  f'{corner + width},{height - radius}')
+        path.push(f'L {corner + width},{height - radius} ' +
+                  f'{corner + width},{radius}')
+        path.push(f'C {corner + width},{radius/2.0} ' +
+                  f'{corner + width - radius/2.0},0 ' +
+                  f'{corner + width - radius},0')
         path.push(f'L {corner + width - radius},0 {corner},0')
         path.push(f'L {corner},0 {corner},{height}')
         path.push('Z')
@@ -61,7 +93,8 @@ def _get_argparser():
     parser.add_argument('--right-width', default=None, nargs='?',
                         help='Width of the left cell, ``None`` if fit to text')
     parser.add_argument('--left-text', default='test', nargs='?',
-                        help='Text for the left cell. Can be python format string')
+                        help='Text for the left cell. Can be python format ' +
+                        'string')
     parser.add_argument('--left-stroke', default='#000000', nargs='?',
                         help='Outline color for left cell.')
     parser.add_argument('--left-fill', default='#000000', nargs='?',
@@ -69,7 +102,8 @@ def _get_argparser():
     parser.add_argument('--left-text-color', default='#FFFFFF', nargs='?',
                         help='Text color for left cell.')
     parser.add_argument('--right-text', default='100%', nargs='?',
-                        help='Text for the right cell.  Can be python format string')
+                        help='Text for the right cell.  Can be python format ' +
+                        'string')
     parser.add_argument('--right-stroke', default='#000000', nargs='?',
                         help='Outline color for right cell.')
     parser.add_argument('--right-fill', default='#FFFFFF', nargs='?',
